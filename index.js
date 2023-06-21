b92d6abfcb428859fd3c42a54281a6d8adf10532
Bikrom-Roy <bikromroy2001@gmail.com>
2023-06-21 22:47:14 +0600

    Email js free using

---
+++ index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "YOUR_SERVICE_ID";
+  const templateID = "YOUR_TEMPLETE_ID";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((error) => console.log(error));
+  console.log(params);
+}
